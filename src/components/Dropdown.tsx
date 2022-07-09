b5f8193e483c8947cfdf71d75cf59b064e2cccb2
kumankoishi.py <7iggif@naver.com>
2022-07-09 22:30:58 +0900

    feat(dropdown): styled menulist

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,17 +1,32 @@
 import { Select as RawSelect, MenuItem } from '@mui/material';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
-const Select = styled(RawSelect)`
+interface SelectProps {
+  isOpen: boolean;
+}
+
+const Select = styled(RawSelect)<SelectProps>`
   box-sizing: border-box;
   width: 400px;
   background: white;
+
+  &.MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline {
+    border: 1px solid #00cccc;
+  }
 
   &:hover .MuiOutlinedInput-notchedOutline {
     border: 2px solid #20cc20;
   }
 
+  &.Mui-focused {
+    border-bottom-left-radius: ${({ isOpen }) => (isOpen ? '0' : '4px')};
+    border-bottom-right-radius: ${({ isOpen }) => (isOpen ? '0' : '4px')};
+  }
+
   &.Mui-focused .MuiOutlinedInput-notchedOutline {
-    border: 2px solid #20cc20;
+    border: ${({ isOpen }) => (isOpen ? '2px solid #20cc20' : '1px solid #00cccc')};
+    border-bottom: ${({ isOpen }) => (isOpen ? 'none' : '')};
   }
 `;
 
@@ -25,14 +40,23 @@ interface DropdownProps {
 }
 
 function Dropdown({ options }: DropdownProps) {
+  const [ isOpen, setIsOpen ] = useState<boolean>(false);
+
   const renderedOptions = options.map(({ value, visual }) => (
     <MenuItem value={value}>
       { visual }
     </MenuItem>
   ));
 
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Select
+      isOpen={isOpen}
+      open={isOpen}
+      onClick={toggleOpen}
       defaultValue='koishi'
       MenuProps={{
         MenuListProps: {
@@ -41,11 +65,21 @@ function Dropdown({ options }: DropdownProps) {
             borderTop: 'none',
             maxHeight: '200px',
             overflowY: 'scroll',
-            borderTopLeftRadius: '0',
-            borderTopRightRadius: '0',
+            boxShadow: 0,
+            padding: 0,
           },
         },
         transitionDuration: 0,
+        sx: {
+          boxShadow: 0,
+        },
+        PaperProps: {
+          sx: {
+            boxShadow: 0,
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+          },
+        },
       }}
     >
       { renderedOptions }
